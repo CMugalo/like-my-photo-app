a366fbb66d3c950b44ca87a9cb571265f6133f79
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2025-01-10 12:25:45 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LikePhotoApp from "./like-my-photo-app/LikePhotoApp";
+
+function App() {
+  return (
+    <div className="App">
+      <LikePhotoApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Title({ classes, text }) {
+  return (
+    <h1
+      className={
+        !classes ? "title text-center" : classes || "title text-center"
+      }
+    >
+      {!text ? "Title" : text}
+    </h1>
+  );
+}
