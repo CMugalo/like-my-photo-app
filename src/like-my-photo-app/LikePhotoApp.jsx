30603278d11ea0648d37a46566abcb61c3c0b741
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2025-01-22 05:46:10 +0300

    completed card design

--- src/like-my-photo-app/LikePhotoApp.jsx
+++ src/like-my-photo-app/LikePhotoApp.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Title from "../components/Title";
+import {
+  AiFillSmile,
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineComment,
+} from "react-icons/ai";
+
+import Dog from "../like-my-photo-app/img/Dog.png";
 
 export default function LikePhotoApp() {
+  let like = false;
   return (
     <div className="container text-center">
       <Title text={"Like Photo App"} />
@@ -11,12 +20,16 @@ export default function LikePhotoApp() {
         style={{ width: 300, cursor: "pointer" }}
       >
         <div className="card-header fs-xl">
+          <AiFillSmile className="mr-2" />
           <small>Dog</small>
-          <img src="" alt="img" style={{ height: "fit-content" }} />
-          <div
-            className="card-footer fs-xl d-flex"
-            style={{ justifyContent: "space-between" }}
-          ></div>
+        </div>
+        <img src={Dog} alt="img" style={{ height: "fit-content" }} />
+        <div
+          className="card-footer fs-xl d-flex"
+          style={{ justifyContent: "space-between" }}
+        >
+          <AiOutlineComment />{" "}
+          {like ? <AiFillHeart className="text-danger" /> : <AiOutlineHeart />}
         </div>
       </div>
     </div>
